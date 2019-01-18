133f54d275246033d9f762befbe74eb20e54fc61
Ryan Gartner <ryanjgartner@gmail.com>
2019-01-17 18:02:37 -0600

    GifTastic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,21 @@
+function renderButtons() {
+    $
+}
+
+
 $("button").on("click", function() {
   var person = $(this).attr("data-seinfeld");
-
+  
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       person + "&api_key=dc6zaTOxFJmzC&limit=10";
-console.log(queryURL);
+
   $.ajax({
       url: queryURL,
       method: "GET"
   })
       .then(function(response) {
           var results = response.data;
-console.log(response);
+          
           for (var i = 0; i < results.length; i++) {
             if (results[i].rating !== "r") {
                 var gifDiv = $("<div>");
